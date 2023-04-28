27712efe675e01baaf9624896ce6928790284c26
dayjyun <kbarriosdev@gmail.com>
2023-04-28 11:02:10 -0500

    genereated getters and setters for Planet class

--- src/main/java/kbarrios/dev/outerspace/models/Planet.java
+++ src/main/java/kbarrios/dev/outerspace/models/Planet.java
@@ -7,7 +7,7 @@ public class Planet {
    private Long lengthOfYear;
    private Long sizeToEarth;
    private boolean habitable;
-   
+
    public Planet() {}
 
    public Planet(Long id, String name, Long distanceFromSun, Long lengthOfYear, boolean habitable) {
@@ -15,6 +15,46 @@ public class Planet {
       this.name = name;
       this.distanceFromSun = distanceFromSun;
       this.lengthOfYear = lengthOfYear;
+      this.habitable = habitable;
+   }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public Long getDistanceFromSun() {
+      return distanceFromSun;
+   }
+
+   public void setDistanceFromSun(Long distanceFromSun) {
+      this.distanceFromSun = distanceFromSun;
+   }
+
+   public Long getLengthOfYear() {
+      return lengthOfYear;
+   }
+
+   public void setLengthOfYear(Long lengthOfYear) {
+      this.lengthOfYear = lengthOfYear;
+   }
+
+   public boolean isHabitable() {
+      return habitable;
+   }
+
+   public void setHabitable(boolean habitable) {
       this.habitable = habitable;
    }
 
