a567fc2d8f762420e62932291ecd878a4ec1a3ad
dayjyun <kbarriosdev@gmail.com>
2023-04-28 11:15:11 -0500

    created private variables in SolarSystem class

--- src/main/java/kbarrios/dev/outerspace/models/SolarSystem.java
+++ src/main/java/kbarrios/dev/outerspace/models/SolarSystem.java
@@ -1,4 +1,9 @@
 package kbarrios.dev.outerspace.models;
 
 public class SolarSystem {
+   private Long id;
+   private String name;
+   private Long ageInBillions;
+   private String type;
+   private Long sizeComparedToEarth;
 }
