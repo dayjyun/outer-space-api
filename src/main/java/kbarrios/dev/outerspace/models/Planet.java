e143cf595053daf08058cfb2321a2ef6a04698ef
dayjyun <kbarriosdev@gmail.com>
2023-04-28 11:01:56 -0500

    genereated constructors for Planet class

--- src/main/java/kbarrios/dev/outerspace/models/Planet.java
+++ src/main/java/kbarrios/dev/outerspace/models/Planet.java
@@ -7,5 +7,15 @@ public class Planet {
    private Long lengthOfYear;
    private Long sizeToEarth;
    private boolean habitable;
+   
+   public Planet() {}
+
+   public Planet(Long id, String name, Long distanceFromSun, Long lengthOfYear, boolean habitable) {
+      this.id = id;
+      this.name = name;
+      this.distanceFromSun = distanceFromSun;
+      this.lengthOfYear = lengthOfYear;
+      this.habitable = habitable;
+   }
 
 }
