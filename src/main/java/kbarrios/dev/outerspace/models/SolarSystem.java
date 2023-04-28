31bc313b1a43964e5aaa883437347fad863dfd81
dayjyun <kbarriosdev@gmail.com>
2023-04-28 11:15:44 -0500

    generated constructors in SolarSystem class

--- src/main/java/kbarrios/dev/outerspace/models/SolarSystem.java
+++ src/main/java/kbarrios/dev/outerspace/models/SolarSystem.java
@@ -6,4 +6,14 @@ public class SolarSystem {
    private Long ageInBillions;
    private String type;
    private Long sizeComparedToEarth;
+
+   public SolarSystem() {}
+
+   public SolarSystem(Long id, String name, Long ageInBillions, String type, Long sizeComparedToEarth) {
+      this.id = id;
+      this.name = name;
+      this.ageInBillions = ageInBillions;
+      this.type = type;
+      this.sizeComparedToEarth = sizeComparedToEarth;
+   }
 }
