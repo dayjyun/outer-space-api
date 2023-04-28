0ed456982b23dc53a5178d16a40e3b64af95253f
dayjyun <kbarriosdev@gmail.com>
2023-04-28 11:28:40 -0500

    generated constructors for Astronomer class

--- src/main/java/kbarrios/dev/outerspace/models/Astronomer.java
+++ src/main/java/kbarrios/dev/outerspace/models/Astronomer.java
@@ -5,4 +5,13 @@ public class Astronomer {
    private String username;
    private String email;
    private String password;
+
+   public Astronomer() {}
+
+   public Astronomer(Long id, String username, String email, String password) {
+      this.id = id;
+      this.username = username;
+      this.email = email;
+      this.password = password;
+   }
 }
