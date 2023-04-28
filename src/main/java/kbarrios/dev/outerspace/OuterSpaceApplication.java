060f1f5c3b2cde9b03492757de2f397dfa64f6c5
dayjyun <kbarriosdev@gmail.com>
2023-04-28 10:22:54 -0500

    first commit

---
+++ src/main/java/kbarrios/dev/outerspace/OuterSpaceApplication.java
@@ -0,0 +1,13 @@
+package kbarrios.dev.outerspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuterSpaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuterSpaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kbarrios/dev/outerspace/OuterSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package kbarrios.dev.outerspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuterSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
