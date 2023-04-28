f53642d28d95acfc730fbdac8e3f67f3d86373a3
dayjyun <kbarriosdev@gmail.com>
2023-04-28 11:17:01 -0500

    updated column sizeComparedToEarth in Planet class

--- src/main/java/kbarrios/dev/outerspace/models/Planet.java
+++ src/main/java/kbarrios/dev/outerspace/models/Planet.java
@@ -20,77 +20,11 @@ public class Planet {
    private Long lengthOfYear;
 
    @Column
-   private Long sizeToEarth;
+   private Long sizeComparedToEarth;
 
    @Column
    private boolean habitable;
 
    public Planet() {}
 
-   public Planet(Long id, String name, Long distanceFromSun, Long lengthOfYear, boolean habitable) {
-      this.id = id;
-      this.name = name;
-      this.distanceFromSun = distanceFromSun;
-      this.lengthOfYear = lengthOfYear;
-      this.habitable = habitable;
-   }
-
-   public Long getId() {
-      return id;
-   }
-
-   public void setId(Long id) {
-      this.id = id;
-   }
-
-   public String getName() {
-      return name;
-   }
-
-   public void setName(String name) {
-      this.name = name;
-   }
-
-   public Long getDistanceFromSun() {
-      return distanceFromSun;
-   }
-
-   public void setDistanceFromSun(Long distanceFromSun) {
-      this.distanceFromSun = distanceFromSun;
-   }
-
-   public Long getLengthOfYear() {
-      return lengthOfYear;
-   }
-
-   public void setLengthOfYear(Long lengthOfYear) {
-      this.lengthOfYear = lengthOfYear;
-   }
-
-   public Long getSizeToEarth() {
-      return sizeToEarth;
-   }
-
-   public void setSizeToEarth(Long sizeToEarth) {
-      this.sizeToEarth = sizeToEarth;
-   }
-
-   public boolean isHabitable() {
-      return habitable;
-   }
-
-   public void setHabitable(boolean habitable) {
-      this.habitable = habitable;
-   }
-
-   @Override
-   public String toString() {
-      return "Planet{" +
-              "id=" + id +
-              ", name='" + name + '\'' +
-              ", distanceFromSun=" + distanceFromSun +
-              ", lengthOfYear=" + lengthOfYear +
-              ", habitable=" + habitable +
-              '}';
-   }
 }
