11a676d863e54737a33eefcd8ef6d05321904bd9
dayjyun <kbarriosdev@gmail.com>
2023-04-28 12:02:52 -0500

    began SolarSystemController endpoints

--- src/main/java/kbarrios/dev/outerspace/controllers/SolarSystemController.java
+++ src/main/java/kbarrios/dev/outerspace/controllers/SolarSystemController.java
@@ -1,4 +1,26 @@
 package kbarrios.dev.outerspace.controllers;
 
+import kbarrios.dev.outerspace.models.SolarSystem;
+import kbarrios.dev.outerspace.repositories.SolarSystemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/api/solar-systems")
 public class SolarSystemController {
+   private SolarSystemRepository solarSystemRepository;
+
+   @Autowired
+   public void setSolarSystemRepository(SolarSystemRepository solarSystemRepository) {
+      this.solarSystemRepository = solarSystemRepository;
+   }
+
+   @GetMapping(path = "")
+   public List<SolarSystem> getAllSolarSystems() {
+      return solarSystemRepository.findAll();
+   }
 }
