3b3c6a145b718f71a5fda1a345b57f66da276d72
dayjyun <kbarriosdev@gmail.com>
2023-04-28 11:01:32 -0500

    created Planet class with private variables

---
+++ src/main/java/kbarrios/dev/outerspace/models/Planet.java
@@ -0,0 +1,11 @@
+package kbarrios.dev.outerspace.models;
+
+public class Planet {
+   private Long id;
+   private String name;
+   private Long distanceFromSun;
+   private Long lengthOfYear;
+   private Long sizeToEarth;
+   private boolean habitable;
+
+}
