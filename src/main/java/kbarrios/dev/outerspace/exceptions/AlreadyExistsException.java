98671052af8e9b94b984aa07cdaceb36b0d4f8bb
dayjyun <kbarriosdev@gmail.com>
2023-04-28 11:47:07 -0500

    completed AlreadyExistsException class in exceptions package

--- src/main/java/kbarrios/dev/outerspace/exceptions/AlreadyExistsException.java
+++ src/main/java/kbarrios/dev/outerspace/exceptions/AlreadyExistsException.java
@@ -1,4 +1,11 @@
 package kbarrios.dev.outerspace.exceptions;
 
-public class AlreadyExistsException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.CONFLICT)
+public class AlreadyExistsException extends RuntimeException {
+   public AlreadyExistsException(String message) {
+      super(message);
+   }
 }
