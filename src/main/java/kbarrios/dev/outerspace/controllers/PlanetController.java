7fb866647014d5418734b10150158fbc3ef1f150
dayjyun <kbarriosdev@gmail.com>
2023-04-28 16:06:25 -0500

    created GetMapping for getAllPlanets in PlanetController

--- src/main/java/kbarrios/dev/outerspace/controllers/PlanetController.java
+++ src/main/java/kbarrios/dev/outerspace/controllers/PlanetController.java
@@ -1,9 +1,13 @@
 package kbarrios.dev.outerspace.controllers;
 
+import kbarrios.dev.outerspace.models.Planet;
 import kbarrios.dev.outerspace.repositories.PlanetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path="/api/planets")
@@ -13,5 +17,10 @@ public class PlanetController {
    @Autowired
    public void setPlanetRepository(PlanetRepository planetRepository) {
       this.planetRepository = planetRepository;
+   }
+
+   @GetMapping(path = "")
+   public List<Planet> getAllPlanets() {
+      return planetRepository.findAll();
    }
 }
