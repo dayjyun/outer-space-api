cde45f72611a778fe5212b74c314a2bb7f456a47
dayjyun <kbarriosdev@gmail.com>
2023-04-28 11:33:47 -0500

    completed AstronomerRepository in repositories package

--- src/main/java/kbarrios/dev/outerspace/repositories/AstronomerRepository.java
+++ src/main/java/kbarrios/dev/outerspace/repositories/AstronomerRepository.java
@@ -1,4 +1,10 @@
 package kbarrios.dev.outerspace.repositories;
 
-public interface AstronomerRepository {
+import kbarrios.dev.outerspace.models.Astronomer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AstronomerRepository extends JpaRepository<Astronomer, Long> {
+   Astronomer findAstronomerByEmail(String email);
+
+   Astronomer findAstronomerById(Long astronomerId);
 }
