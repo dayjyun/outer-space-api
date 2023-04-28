08293aa08ab5eb8d73793df800e41d17bb9396e4
dayjyun <kbarriosdev@gmail.com>
2023-04-28 11:19:26 -0500

    generated getters and setters for SolarSystem class

--- src/main/java/kbarrios/dev/outerspace/models/SolarSystem.java
+++ src/main/java/kbarrios/dev/outerspace/models/SolarSystem.java
@@ -16,4 +16,44 @@ public class SolarSystem {
       this.type = type;
       this.sizeComparedToEarth = sizeComparedToEarth;
    }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public Long getAgeInBillions() {
+      return ageInBillions;
+   }
+
+   public void setAgeInBillions(Long ageInBillions) {
+      this.ageInBillions = ageInBillions;
+   }
+
+   public String getType() {
+      return type;
+   }
+
+   public void setType(String type) {
+      this.type = type;
+   }
+
+   public Long getSizeComparedToEarth() {
+      return sizeComparedToEarth;
+   }
+
+   public void setSizeComparedToEarth(Long sizeComparedToEarth) {
+      this.sizeComparedToEarth = sizeComparedToEarth;
+   }
 }
