7ceeb3487f8ca4cd3fd1837f2ecc551ca86c9a6d
dayjyun <kbarriosdev@gmail.com>
2023-04-28 16:01:30 -0500

    placed boilerplate code in PlanetController

--- src/main/java/kbarrios/dev/outerspace/controllers/PlanetController.java
+++ src/main/java/kbarrios/dev/outerspace/controllers/PlanetController.java
@@ -1,4 +1,17 @@
 package kbarrios.dev.outerspace.controllers;
 
+import kbarrios.dev.outerspace.repositories.PlanetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path="/api/planets")
 public class PlanetController {
+   private PlanetRepository planetRepository;
+
+   @Autowired
+   public void setPlanetRepository(PlanetRepository planetRepository) {
+      this.planetRepository = planetRepository;
+   }
 }
