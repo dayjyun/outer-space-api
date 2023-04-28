733382e3416fb3a06a5dd8de9552798e5371793c
dayjyun <kbarriosdev@gmail.com>
2023-04-28 11:17:50 -0500

    generated new getters and setters in Planet class

--- src/main/java/kbarrios/dev/outerspace/models/Planet.java
+++ src/main/java/kbarrios/dev/outerspace/models/Planet.java
@@ -35,4 +35,52 @@ public class Planet {
       this.sizeComparedToEarth = sizeComparedToEarth;
       this.habitable = habitable;
    }
+
+   public Long getId() {
+      return id;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public Long getDistanceFromSun() {
+      return distanceFromSun;
+   }
+
+   public void setDistanceFromSun(Long distanceFromSun) {
+      this.distanceFromSun = distanceFromSun;
+   }
+
+   public Long getLengthOfYear() {
+      return lengthOfYear;
+   }
+
+   public void setLengthOfYear(Long lengthOfYear) {
+      this.lengthOfYear = lengthOfYear;
+   }
+
+   public Long getSizeComparedToEarth() {
+      return sizeComparedToEarth;
+   }
+
+   public void setSizeComparedToEarth(Long sizeComparedToEarth) {
+      this.sizeComparedToEarth = sizeComparedToEarth;
+   }
+
+   public boolean isHabitable() {
+      return habitable;
+   }
+
+   public void setHabitable(boolean habitable) {
+      this.habitable = habitable;
+   }
 }
