00b42ba6b01a438fdf5ffd88fc5427ff8015a0b0
dayjyun <kbarriosdev@gmail.com>
2023-04-29 01:55:11 -0500

    Completed getAllPlanets endpoint for PlanetController and PlanetService

--- src/main/java/kbarrios/dev/outerspace/service/PlanetService.java
+++ src/main/java/kbarrios/dev/outerspace/service/PlanetService.java
@@ -1,7 +1,22 @@
 package kbarrios.dev.outerspace.service;
 
+import kbarrios.dev.outerspace.models.Planet;
+import kbarrios.dev.outerspace.repositories.PlanetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PlanetService {
+   private PlanetRepository planetRepository;
+
+   @Autowired
+   public void setPlanetRepository(PlanetRepository planetRepository) {
+      this.planetRepository = planetRepository;
+   }
+
+   public List<Planet> getAllPlanets(){
+      return planetRepository.findAll();
+   }
 }
