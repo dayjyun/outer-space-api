bedca624cc1703d315a67f4f35f962309e9781f2
dayjyun <kbarriosdev@gmail.com>
2023-04-30 00:04:49 -0500

    removed version for spring boot starter security and updated AstronomerService to import required dependencies for password encoding

--- src/main/java/kbarrios/dev/outerspace/service/AstronomerService.java
+++ src/main/java/kbarrios/dev/outerspace/service/AstronomerService.java
@@ -1,4 +1,16 @@
 package kbarrios.dev.outerspace.service;
 
+import kbarrios.dev.outerspace.repositories.AstronomerRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AstronomerService {
+   private final AstronomerRepository astronomerRepository;
+   private final PasswordEncoder passwordEncoder;
+
+   public AstronomerService(AstronomerRepository astronomerRepository, PasswordEncoder passwordEncoder) {
+      this.astronomerRepository = astronomerRepository;
+      this.passwordEncoder = passwordEncoder;
+   }
 }
