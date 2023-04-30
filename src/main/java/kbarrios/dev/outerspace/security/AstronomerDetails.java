8f137f8127bc2f820d59a6586265c9643f8b691e
dayjyun <kbarriosdev@gmail.com>
2023-04-30 00:35:25 -0500

    Completed AstronomerDetails in security package

--- src/main/java/kbarrios/dev/outerspace/security/AstronomerDetails.java
+++ src/main/java/kbarrios/dev/outerspace/security/AstronomerDetails.java
@@ -1,4 +1,54 @@
 package kbarrios.dev.outerspace.security;
 
-public class AstronomerDetails {
+import kbarrios.dev.outerspace.models.Astronomer;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class AstronomerDetails implements UserDetails {
+   private Astronomer astronomer;
+
+   public AstronomerDetails(Astronomer astronomer) {
+      this.astronomer = astronomer;
+   }
+
+   @Override
+   public Collection<? extends GrantedAuthority> getAuthorities() {
+      return null;
+   }
+
+   @Override
+   public String getPassword() {
+      return null;
+   }
+
+   @Override
+   public String getUsername() {
+      return null;
+   }
+
+   @Override
+   public boolean isAccountNonExpired() {
+      return false;
+   }
+
+   @Override
+   public boolean isAccountNonLocked() {
+      return false;
+   }
+
+   @Override
+   public boolean isCredentialsNonExpired() {
+      return false;
+   }
+
+   @Override
+   public boolean isEnabled() {
+      return false;
+   }
+
+   public Astronomer getAstronomer() {
+      return astronomer;
+   }
 }
