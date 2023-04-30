e62db90c57e9570d227405a2b9b2b6e3e6e1802b
dayjyun <kbarriosdev@gmail.com>
2023-04-30 01:19:27 -0500

    Completed AstronomerService in service package

--- src/main/java/kbarrios/dev/outerspace/service/AstronomerService.java
+++ src/main/java/kbarrios/dev/outerspace/service/AstronomerService.java
@@ -2,8 +2,19 @@ package kbarrios.dev.outerspace.service;
 
 import kbarrios.dev.outerspace.exceptions.AlreadyExistsException;
 import kbarrios.dev.outerspace.models.Astronomer;
+import kbarrios.dev.outerspace.models.login.LoginRequest;
+import kbarrios.dev.outerspace.models.login.LoginResponse;
 import kbarrios.dev.outerspace.repositories.AstronomerRepository;
+import kbarrios.dev.outerspace.security.AstronomerDetails;
+import kbarrios.dev.outerspace.security.AstronomerDetailsService;
+import kbarrios.dev.outerspace.security.JWTUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +22,21 @@ import org.springframework.stereotype.Service;
 public class AstronomerService {
    private final AstronomerRepository astronomerRepository;
    private final PasswordEncoder passwordEncoder;
+   private final JWTUtils jwtUtils;
+   private final AuthenticationManager authenticationManager;
+   private AstronomerDetailsService astronomerDetailsService;
 
    @Autowired
-   public AstronomerService(AstronomerRepository astronomerRepository, PasswordEncoder passwordEncoder) {
+   public AstronomerService(AstronomerRepository astronomerRepository,
+                            @Lazy PasswordEncoder passwordEncoder,
+                            JWTUtils jwtUtils,
+                            @Lazy AuthenticationManager authenticationManager,
+                            @Lazy AstronomerDetailsService astronomerDetailsService) {
       this.astronomerRepository = astronomerRepository;
       this.passwordEncoder = passwordEncoder;
+      this.jwtUtils = jwtUtils;
+      this.authenticationManager = authenticationManager;
+      this.astronomerDetailsService = astronomerDetailsService;
    }
 
    public Astronomer createAstronomer(Astronomer astronomerObject) {
@@ -29,5 +50,19 @@ public class AstronomerService {
 
    public Astronomer findAstronomerByEmail(String email) {
       return astronomerRepository.findAstronomerByEmail(email);
+   }
+
+   public ResponseEntity<?> loginUser(LoginRequest loginRequest) {
+      UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword());
+      try {
+         Authentication authentication = authenticationManager
+                 .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+         SecurityContextHolder.getContext().setAuthentication(authentication);
+          astronomerDetails = (AstronomerDetails) authentication.getPrincipal();
+         final String JWT = jwtUtils.generateJwtToken(astronomerDetails);
+         return ResponseEntity.ok(new LoginResponse(JWT));
+      } catch (Exception e) {
+         return ResponseEntity.ok(new LoginResponse("Error : user name or password is incorrect"));
+      }UsernamePasswordAuthenticationToken
    }
 }
