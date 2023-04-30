63c940af8e50667534c8d013fb2f715c391b2c90
dayjyun <kbarriosdev@gmail.com>
2023-04-30 00:12:36 -0500

    completed AstronomerService in service package

--- src/main/java/kbarrios/dev/outerspace/service/AstronomerService.java
+++ src/main/java/kbarrios/dev/outerspace/service/AstronomerService.java
@@ -1,6 +1,9 @@
 package kbarrios.dev.outerspace.service;
 
+import kbarrios.dev.outerspace.exceptions.AlreadyExistsException;
+import kbarrios.dev.outerspace.models.Astronomer;
 import kbarrios.dev.outerspace.repositories.AstronomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -9,8 +12,22 @@ public class AstronomerService {
    private final AstronomerRepository astronomerRepository;
    private final PasswordEncoder passwordEncoder;
 
+   @Autowired
    public AstronomerService(AstronomerRepository astronomerRepository, PasswordEncoder passwordEncoder) {
       this.astronomerRepository = astronomerRepository;
       this.passwordEncoder = passwordEncoder;
+   }
+
+   public Astronomer createAstronomer(Astronomer astronomerObject) {
+      if(!astronomerRepository.existsAstronomerByEmail(astronomerObject.getEmail())) {
+         astronomerObject.setPassword(passwordEncoder.encode(astronomerObject.getPassword()));
+         return astronomerRepository.save(astronomerObject);
+      } else {
+         throw new AlreadyExistsException("Email address " + astronomerObject.getEmail() + " already exists");
+      }
+   }
+
+   public Astronomer findAstronomerByEmail(String email) {
+      return astronomerRepository.findAstronomerByEmail(email);
    }
 }
