848b1ffd22d5e3385f5ee7999245d54bdf5e2b98
dayjyun <kbarriosdev@gmail.com>
2023-05-01 00:15:10 -0500

    Removed unused code in AstronomerRepository

--- src/main/java/kbarrios/dev/outerspace/repositories/AstronomerRepository.java
+++ src/main/java/kbarrios/dev/outerspace/repositories/AstronomerRepository.java
@@ -4,8 +4,6 @@ import kbarrios.dev.outerspace.models.Astronomer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AstronomerRepository extends JpaRepository<Astronomer, Long> {
-//   Astronomer findAstronomerById(Long astronomerId);
-
    Astronomer findAstronomerByEmail(String email);
 
    boolean existsAstronomerByEmail(String email);
