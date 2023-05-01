5f10cd000ed6d39eed1f541e403480f929d7f610
dayjyun <kbarriosdev@gmail.com>
2023-05-01 00:08:33 -0500

    Removed unused code in SolarSystemRepositry in repositories package

--- src/main/java/kbarrios/dev/outerspace/repositories/SolarSystemRepository.java
+++ src/main/java/kbarrios/dev/outerspace/repositories/SolarSystemRepository.java
@@ -3,12 +3,9 @@ package kbarrios.dev.outerspace.repositories;
 import kbarrios.dev.outerspace.models.SolarSystem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface SolarSystemRepository extends JpaRepository<SolarSystem, Long> {
-   Optional<SolarSystem> findByName(String name);
-
    Optional<SolarSystem> findSolarSystemByNameAndAstronomerId(String solarSystemName, Long astronomerId);
 
    Optional<SolarSystem> findSolarSystemByIdAndAstronomerId(Long solarSystemId, Long astronomerId);
