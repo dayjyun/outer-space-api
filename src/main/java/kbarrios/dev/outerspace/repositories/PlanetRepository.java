da8d5f9e8375d797ae3063bdac20c0d5cdddf28a
dayjyun <kbarriosdev@gmail.com>
2023-05-01 00:10:16 -0500

    removed unused code from PlanetRepository in repositories

--- src/main/java/kbarrios/dev/outerspace/repositories/PlanetRepository.java
+++ src/main/java/kbarrios/dev/outerspace/repositories/PlanetRepository.java
@@ -3,12 +3,9 @@ package kbarrios.dev.outerspace.repositories;
 import kbarrios.dev.outerspace.models.Planet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public interface PlanetRepository extends JpaRepository<Planet, Long> {
-   Optional<Planet> findPlanetByName(String planetName);
-
    Optional<Planet> findPlanetByNameAndAstronomerId(String planetName, Long astronomerId);
 
    Optional<Planet> findPlanetByIdAndAstronomerId(Long planetId, Long astronomerId);
